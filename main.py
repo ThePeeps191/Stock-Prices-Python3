731ae90347afa796ae9e2021c00b627a07b0b01f
The Peeps191 <thepeeps.wang@gmail.com>
2021-02-01 16:44:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests, json
+
+def price(sym:str):
+	s = json.loads(requests.get("http://flask-stock-prices-python.thepeeps191.repl.co/price?sym="+sym).text)
+	return s
+
+# Cenovus Energy
+cve = price("cve")
+print(cve['price'])
+
+'''
+{'detail': 'valid', 'symbol': 'CVE', 'price': '6.11'}
+'''
